d619cb41461ef1764a29dc9b5654162485bb8f23
nereya <nereya800@gmail.com>
2024-05-13 16:06:57 +0300

    fff

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const https = require('https');
 const fs = require('fs');
 const app = express();
-const PORT = 3000;
+const PORT = 5500;
 
 // HTTPS options
 const httpsOptions = {
