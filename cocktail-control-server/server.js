399e0bb92bfe9ab36808c0e8206c5d121ddf6c37
nereya <nereya800@gmail.com>
2024-05-13 17:46:33 +0300

    enddd

--- cocktail-control-server/server.js
+++ cocktail-control-server/server.js
@@ -1,22 +1,19 @@
 const express = require('express');
-const http = require('http'); // Use 'http' module for HTTP requests
-const fs = require('fs');
+const http = require('http');
 const app = express();
 const PORT = 5500;
 
 // Define routes
-app.get('/turn-on', (req, res) => {
+app.post('/turn-on', (req, res) => {
     // Send HTTP request to ESP8266 to turn on the lightbulb
-    // Use 'http' module instead of 'https'
     http.get('http://192.168.1.125/turn-on', (response) => {
         console.log('Lightbulb turned on');
         res.send('Lightbulb turned on');
     });
 });
 
-app.get('/turn-off', (req, res) => {
+app.post('/turn-off', (req, res) => {
     // Send HTTP request to ESP8266 to turn off the lightbulb
-    // Use 'http' module instead of 'https'
     http.get('http://192.168.1.125/turn-off', (response) => {
         console.log('Lightbulb turned off');
         res.send('Lightbulb turned off');
