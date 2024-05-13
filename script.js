918cd8fab24e8edb858eab1f1a61b0572ad3a5af
nereya <nereya800@gmail.com>
2024-05-14 00:11:43 +0300

    fea

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let url = 'https://1f09-77-127-173-136.ngrok-free.app'
+let url = 'https://2f09-77-127-173-136.ngrok-free.app'
 function turnOn() {
     fetch(url + '/turn-on', { method: 'POST' })
         .then(response => console.log(response))
