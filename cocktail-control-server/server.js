aa0bd69f1a7cb3820760d844c55259f2b8bf1577
nereya <nereya800@gmail.com>
2024-05-13 16:26:22 +0300

    s

--- cocktail-control-server/server.js
+++ cocktail-control-server/server.js
@@ -1,20 +1,14 @@
 const express = require('express');
-const https = require('https');
+const http = require('http'); // Use 'http' module for HTTP requests
 const fs = require('fs');
 const app = express();
 const PORT = 5500;
 
-// HTTPS options
-const httpsOptions = {
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert')
-};
-
 // Define routes
 app.get('/turn-on', (req, res) => {
     // Send HTTP request to ESP8266 to turn on the lightbulb
-    // Replace 'esp8266-ip' with the IP address of your ESP8266
-    https.get('http://192.168.1.125/turn-on', (response) => {
+    // Use 'http' module instead of 'https'
+    http.get('http://192.168.1.125/turn-on', (response) => {
         console.log('Lightbulb turned on');
         res.send('Lightbulb turned on');
     });
@@ -22,14 +16,14 @@ app.get('/turn-on', (req, res) => {
 
 app.get('/turn-off', (req, res) => {
     // Send HTTP request to ESP8266 to turn off the lightbulb
-    // Replace 'esp8266-ip' with the IP address of your ESP8266
-    https.get('http://192.168.1.125/turn-off', (response) => {
+    // Use 'http' module instead of 'https'
+    http.get('http://192.168.1.125/turn-off', (response) => {
         console.log('Lightbulb turned off');
         res.send('Lightbulb turned off');
     });
 });
 
 // Start the server
-https.createServer(httpsOptions, app).listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
