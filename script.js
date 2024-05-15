df62ce903329a223489d82729b7b51aaf6ab68e6
nereya <nereya800@gmail.com>
2024-05-16 00:14:18 +0300

    ssd

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let url = 'https://d5ab-77-127-173-136.ngrok-free.app'
+let url = 'https://bdfc-77-127-173-136.ngrok-free.app'
 
 function turnOn() {
     fetch(url + '/turn-on', { method: 'POST' })
