e8a6b177a9190002c3e69e268905bf12aea85d6a
nereya <nereya800@gmail.com>
2024-05-17 00:05:18 +0300

    go back

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+let url = 'https://48ff-77-127-173-136.ngrok-free.app'
+
+function turnOn() {
+    fetch(url + '/turn-on', { method: 'POST' })
+        .then(response => console.log(response))
+        .catch(error => console.error(error));
+}
+
+function turnOff() {
+    fetch(url + '/turn-off', { method: 'POST' })
+        .then(response => console.log(response))
+        .catch(error => console.error(error));
+}
+
 function toggleOptions(event, cocktailName) {
     let clickedOptions = event.currentTarget.querySelector(".options");
 
@@ -21,10 +35,10 @@ var orders = document.querySelectorAll(".cocktailOrder");
 orders.forEach(function (element) {
     element.addEventListener("click", orderThis);
 });
+
 function orderThis(event) {
     alert(event.currentTarget.getAttribute("id"));
     var cocktailName = event.currentTarget.getAttribute("id");
     window.location.href =
         "https://nereyamantzur.github.io/CocktailApp/" + cocktailName + ".html";
 }
-
