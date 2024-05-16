f44b93e435dc7bb783337f5d9613023f1e12c7c8
nereya <nereya800@gmail.com>
2024-05-17 00:01:43 +0300

    okok

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
-// WebSocket connection to ESP8266 server
-const webSocket = new WebSocket("ws://<ESP8266_IP>:81");
-
-webSocket.onopen = function (event) {
-    console.log("WebSocket connection established");
-};
-
-webSocket.onmessage = function (event) {
-    console.log("Message received:", event.data);
-    // Additional logic for handling messages from ESP8266
-};
-
-webSocket.onclose = function (event) {
-    console.log("WebSocket connection closed");
-};
-
 function toggleOptions(event, cocktailName) {
     let clickedOptions = event.currentTarget.querySelector(".options");
 
     if (clickedOptions.style.display === "block") {
-        clickedOptions.style.display = "none"; // Close options if already open
     } else {
         // Close options for all items
         let allOptions = document.querySelectorAll(".options");
@@ -38,13 +21,10 @@ var orders = document.querySelectorAll(".cocktailOrder");
 orders.forEach(function (element) {
     element.addEventListener("click", orderThis);
 });
-
 function orderThis(event) {
+    alert(event.currentTarget.getAttribute("id"));
     var cocktailName = event.currentTarget.getAttribute("id");
-    console.log("Ordering cocktail: " + cocktailName);
-    // Send a message to ESP8266 to order the selected cocktail
-    webSocket.send(cocktailName);
-
-    // Redirect to the cocktail page
-    window.location.href = "https://nereyamantzur.github.io/CocktailApp/" + cocktailName + ".html";
+    window.location.href =
+        "https://nereyamantzur.github.io/CocktailApp/" + cocktailName + ".html";
 }
+
