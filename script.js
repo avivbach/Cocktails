973fbbc315a04eee83f75f7119eb2097ef174e30
nereya <nereya800@gmail.com>
2024-05-16 23:23:19 +0300

    try

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-let url = 'https://48ff-77-127-173-136.ngrok-free.app'
+// WebSocket connection to ESP8266 server
+const webSocket = new WebSocket("ws://192.168.1.217:443");
 
-function turnOn() {
-    fetch(url + '/turn-on', { method: 'POST' })
-        .then(response => console.log(response))
-        .catch(error => console.error(error));
-}
+webSocket.onopen = function (event) {
+    console.log("WebSocket connection established");
+};
 
-function turnOff() {
-    fetch(url + '/turn-off', { method: 'POST' })
-        .then(response => console.log(response))
-        .catch(error => console.error(error));
-}
+webSocket.onmessage = function (event) {
+    console.log("Message received:", event.data);
+    // Additional logic for handling messages from ESP8266
+};
+
+webSocket.onclose = function (event) {
+    console.log("WebSocket connection closed");
+};
 
 function toggleOptions(event, cocktailName) {
     let clickedOptions = event.currentTarget.querySelector(".options");
 
     if (clickedOptions.style.display === "block") {
+        clickedOptions.style.display = "none"; // Close options if already open
     } else {
         // Close options for all items
         let allOptions = document.querySelectorAll(".options");
@@ -37,8 +40,11 @@ orders.forEach(function (element) {
 });
 
 function orderThis(event) {
-    alert(event.currentTarget.getAttribute("id"));
     var cocktailName = event.currentTarget.getAttribute("id");
-    window.location.href =
-        "https://nereyamantzur.github.io/CocktailApp/" + cocktailName + ".html";
+    console.log("Ordering cocktail: " + cocktailName);
+    // Send a message to ESP8266 to order the selected cocktail
+    webSocket.send(cocktailName);
+
+    // Redirect to the cocktail page
+    window.location.href = "https://nereyamantzur.github.io/CocktailApp/" + cocktailName + ".html";
 }
