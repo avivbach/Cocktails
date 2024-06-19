83ee800c7b12d5bc3334f88ee7a37f2f52a52e00
nereya <nereya800@gmail.com>
2024-06-19 17:14:28 +0300

    kkkk

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function toggleLight(state) {
-    fetch(`http://192.168.1.175/${state}`)
-        .then(response => response.text())
+    fetch(`http://192.168.1.175/${state}`, {
+        method: 'GET',
+        mode: 'cors'
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
         .then(data => console.log(data))
         .catch(error => console.error('Error:', error));
 }
