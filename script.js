458ac74a804bd03b7e4590ac577cff4912e67bbf
nereya <nereya800@gmail.com>
2024-09-03 00:20:28 +0300

    test

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function toggleLight(state) {
-    fetch(`http://192.168.1.175/${state}`)
+const esp32_ip = 'http://192.168.1.178';  // Replace with your ESP32's IP address
+
+function turnOn() {
+    fetch(`${esp32_ip}/turn-on`)
         .then(response => response.text())
-        .then(data => console.log(data))
+        .then(data => {
+            document.getElementById('status').innerText = "LED is ON";
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+function turnOff() {
+    fetch(`${esp32_ip}/turn-off`)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('status').innerText = "LED is OFF";
+        })
         .catch(error => console.error('Error:', error));
 }
