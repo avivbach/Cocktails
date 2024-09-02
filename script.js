72e663d1a1827678e6928517a66b4691bf045bf2
nereya <nereya800@gmail.com>
2024-09-03 01:27:57 +0300

    t

--- script.js
+++ script.js
@@ -3,13 +3,36 @@ const esp32_ip = 'http://192.168.1.178';
 
 function margarita() {
     fetch(`${esp32_ip}/margarita`)
-
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('status').innerText = "LED is ON";
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            document.getElementById('status').innerText = "Error: Unable to turn on LED";
+        });
 }
 
 function martini() {
     fetch(`${esp32_ip}/martini`)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('status').innerText = "LED is ON";
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            document.getElementById('status').innerText = "Error: Unable to turn on LED";
+        });
 }
 
 function turnOff() {
     fetch(`${esp32_ip}/turn-off`)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('status').innerText = "LED is OFF";
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            document.getElementById('status').innerText = "Error: Unable to turn off LED";
+        });
 }
